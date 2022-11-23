dd8eb1ed10f42191dcb34b7f4098e8f755e7cb65
andrii.korpusov <andrii.korpusov@gmail.com>
2022-11-23 11:44:55 +0200

    update nginx, install packages

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -1,10 +1,18 @@
 <?php
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
- * @ORM\Entity
+ * @ApiResource()
+ * @ApiFilter(BooleanFilter::class, properties={"isActive"})
+ * @ORM\Entity(repositoryClass="App\Repository\CompanyRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Company
 {
@@ -16,11 +24,158 @@ class Company
     private $id;
 
     /**
+     * @ORM\Column(type="string")
+     * @var $name string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var $url string
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var $createdAt \DateTime
+     * @ApiProperty(writable=false)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var $updatedAt \DateTime
+     * @ApiProperty(writable=false)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default":1})
+     * @var $isActive boolean
+     */
+    private $isActive;
+
+    /**
      * @return mixed
      */
     public function getId()
     {
         return $this->id;
     }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return self
+     */
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     * @return self
+     */
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     * @return self
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param bool $isActive
+     * @return self
+     */
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @return void
+     */
+    public function prePersist(): void
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     * @return void
+     */
+    public function preUpdate(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+
 
 }
